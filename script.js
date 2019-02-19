face7234c49dcca63debe20f7337d4980748405a
veridiem <jessicavga@gmail.com>
2019-02-19 16:46:57 -0300

    Iniciando Twitter

---
+++ script.js
@@ -0,0 +1,9 @@
+let tweetsBox = document.querySelector("#boxarea");
+let tweetButton = document.querySelector("#tweetButton");
+
+document.getElementsById("boxarea").addEventListener("click", funcao);
+
+function toTweet(){
+
+}
+
